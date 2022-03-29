1242a92db7ae78739b2d7c5445964a787be55506
Wen Low <chawenmushu@gmail.com>
2022-03-29 14:13:32 +0800

    Mod v2 with last number

--- for.go
+++ for.go
@@ -3,12 +3,16 @@ package main
 import "fmt"
 
 func main() {
-	for i := 1; i <= 100; i++ {
-		fmt.Println(i)
+	for i := 1; i <= 10; i++ {
+		// fmt.Println(i)
 		if i%2 == 0 {
-
+			fmt.Println("Even")
 		} else {
-
+			fmt.Println("Odd")
 		}
+
+		fmt.Println(i)
 	}
+
+	fmt.Println("Last number")
 }
