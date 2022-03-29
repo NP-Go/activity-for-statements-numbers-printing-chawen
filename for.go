0fb064eff254a76eac5e5b90f51009e290ebc24a
Wen Low <chawenmushu@gmail.com>
2022-03-29 14:22:08 +0800

    Commit input

--- for.go
+++ for.go
@@ -3,7 +3,12 @@ package main
 import "fmt"
 
 func main() {
-	for i := 1; i <= 10; i++ {
+	var input1, input2 int
+	fmt.Println("Enter first scan")
+	fmt.Scanln(&input1)
+	fmt.Println("Enter second scan")
+	fmt.Scanln(&input2)
+	for i := input1; i <= input2; i++ {
 		// fmt.Println(i)
 		if i%2 == 0 {
 			fmt.Println("Even")
